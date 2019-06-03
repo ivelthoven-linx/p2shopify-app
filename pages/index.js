aef036abd94fd5cfd05b2dc641b9ee47a2e6c724
ivelthoven-linx <51231539+ivelthoven-linx@users.noreply.github.com>
2019-06-03 09:18:49 +0200

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ import {
 } from "@shopify/polaris";
 import React from "react";
 // import { Link } from "@shopify/polaris/types/components/AppProvider/utilities";
-
+// sdfsssss
 class Index extends React.Component {
   render() {
     return (
